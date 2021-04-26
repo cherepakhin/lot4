7836ff46f08b71d594b3835568aaf1f9c025ca1b
Черепахин Василий <vasi.che@gmail.com>
2021-04-26 13:58:02 +0500

    Spring init

---
+++ src/main/java/ru/stm/lot4/TestTaskForLot4Application.java
@@ -0,0 +1,13 @@
+package ru.stm.lot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForLot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskForLot4Application.class, args);
+	}
+
+}
