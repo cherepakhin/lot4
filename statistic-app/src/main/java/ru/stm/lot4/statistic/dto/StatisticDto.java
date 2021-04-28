f039bfcff9363899b3f593cc7e0897f0d7c336c1
Lezhachev Aleksandr <43756493+Aleksandr-Lezhachev@users.noreply.github.com>
2021-04-28 17:26:26 +0300

    Добавлена документация к statistic-app

--- statistic-app/src/main/java/ru/stm/lot4/statistic/dto/StatisticDto.java
+++ statistic-app/src/main/java/ru/stm/lot4/statistic/dto/StatisticDto.java
@@ -1,5 +1,7 @@
 package ru.stm.lot4.statistic.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,12 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
+@ApiModel(description = "Модель информации по статистике версии приложения")
 public class StatisticDto {
+    @ApiModelProperty(dataType = "String", name = "version", notes = "Версия приложения")
     private String version;
+    @ApiModelProperty(dataType = "Long", name = "registrationsCount", notes = "Количество зарегистрированных пользователей")
     private Long registrationsCount;
+    @ApiModelProperty(dataType = "Long", name = "phoneNumbersCount", notes = "Количество уникальных номеров телефона")
     private Long phoneNumbersCount;
 }
