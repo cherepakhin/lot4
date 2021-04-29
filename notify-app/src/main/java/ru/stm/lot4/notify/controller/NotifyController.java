84582e0b256ffc3de60a384cdadc5d471d5b4eb3
Borisov <aleksander.borisov@stm-labs.ru>
2021-04-29 15:16:40 +0300

    refactoring notify-app

--- notify-app/src/main/java/ru/stm/lot4/notify/controller/NotifyController.java
+++ notify-app/src/main/java/ru/stm/lot4/notify/controller/NotifyController.java
@@ -7,9 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.stm.lot4.dto.PushNotificationRequest;
-import ru.stm.lot4.notify.service.impl.NotifyServiceImpl;
+import ru.stm.lot4.notify.service.NotifyService;
 
 import javax.validation.Valid;
 
@@ -20,7 +24,7 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "*")
 public class NotifyController {
 
-    private final NotifyServiceImpl notifyService;
+    private final NotifyService notifyService;
 
     @PostMapping(value = "/create")
     @ApiOperation(
