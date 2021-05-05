d6cb9febd6926577abfbb7e583c33cb038454103
Borisov <aleksander.borisov@stm-labs.ru>
2021-05-05 20:17:53 +0300

    Add Spring Cloud.

--- front-app/src/app/enums/errors.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum ErrorsEnum {
-  LOADING_ERROR = 'An error occurred while loading data',
-  NO_MESSAGES_ERROR = 'No messages found for this phone number',
-  NO_VERSIONS_ERROR = 'No versions found',
-}
---
+++ frontend/src/app/enums/errors.enum.ts
@@ -0,0 +1,5 @@
+export enum ErrorsEnum {
+  LOADING_ERROR = 'An error occurred while loading data',
+  NO_MESSAGES_ERROR = 'No messages found for this phone number',
+  NO_VERSIONS_ERROR = 'No versions found',
+}
